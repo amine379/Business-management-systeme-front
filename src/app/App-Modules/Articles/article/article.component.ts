b33fdb6b78331d31fff1501ae8363e1b1a8c65fa
Amine Baggar <baggaramine1@gmail.com>
2023-09-27 03:13:50 +0200

    add excel table to articleComponent

--- src/app/App-Modules/Articles/article/article.component.ts
+++ src/app/App-Modules/Articles/article/article.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-article',
@@ -9,5 +10,12 @@ export class ArticleComponent {
   public isAddToArticleVisible: boolean=false ;
   modalVisible(){
    this.isAddToArticleVisible=!this.isAddToArticleVisible;
+  }
+  exportToExcel(): void {
+    const table = document.getElementById('my-table'); // Replace 'my-table' with the ID of your table
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(table);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    XLSX.writeFile(wb, 'BMSbyAmine.xlsx'); // You can change the file name as needed
   }
 }
